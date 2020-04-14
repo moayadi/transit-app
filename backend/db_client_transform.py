8d558a893d4f6361e398c55ef5ad8a2072201e40
assareh <assareh@hashicorp.com>
2020-04-14 11:13:30 -0700

    fixed seed SSNs with correct encoded values

--- backend/db_client_transform.py
+++ backend/db_client_transform.py
@@ -21,9 +21,9 @@ CREATE TABLE IF NOT EXISTS `customers` (
 
 seed_customers = '''
 INSERT IGNORE into customers VALUES
-  (2, "3/14/69", "Larry", "Johnson", "2020-01-01T14:49:12.301977", "111-22-3333", "Tyler, Texas", "7000000"),
-  (40, "11/26/69", "Shawn", "Kemp", "2020-02-21T10:24:55.985726", "222-33-4444", "Elkhart, Indiana", "15000000"),
-  (34, "2/20/63", "Charles", "Barkley", "2019-04-09T01:10:20.548144", "333-44-5555", "Leeds, Alabama", "9000000");
+  (2, "3/14/69", "Larry", "Johnson", "2020-01-01T14:49:12.301977", "360-56-6750", "Tyler, Texas", "7000000"),
+  (40, "11/26/69", "Shawn", "Kemp", "2020-02-21T10:24:55.985726", "235-32-8091", "Elkhart, Indiana", "15000000"),
+  (34, "2/20/63", "Charles", "Barkley", "2019-04-09T01:10:20.548144", "531-72-1553", "Leeds, Alabama", "9000000");
 '''
 
 logger = logging.getLogger(__name__)
